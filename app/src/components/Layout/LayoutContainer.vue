fad1da31a174fb2af368b1630d739d4f9ec20250
Vitor Luiz Cavalcanti <vitorluizc@outlook.com>
2018-06-09 18:16:16 -0300

    Create Layout components

---
+++ app/src/components/Layout/LayoutContainer.vue
@@ -0,0 +1,14 @@
+<template>
+  <section>
+    <layout-header />
+    <slot />
+  </section>
+</template>
+
+<script>
+  import LayoutHeader from '@/components/Layout/LayoutHeader';
+
+  export default {
+    components: { LayoutHeader },
+  };
+</script>
---
+++ app/src/components/Layout/LayoutHeader.vue
@@ -0,0 +1,60 @@
+<template>
+  <header class="LayoutHeader">
+    <div class="container">
+      <img src="~@/assets/images/Logo.png" class="figure" alt="" />
+
+      <layout-menu />
+
+      <impact-button
+        type="button"
+        class="button"
+        @click="$router.push('/sign-in')"
+      >
+        Entrar
+      </impact-button>
+    </div>
+  </header>
+</template>
+
+<script>
+  import ImpactButton from '@/components/Impact/ImpactButton';
+  import LayoutMenu from '@/components/Layout/LayoutMenu';
+
+  export default {
+    components: { LayoutMenu, ImpactButton },
+    data () {
+      return {
+        items
+      };
+    }
+  };
+</script>
+
+<style lang="stylus">
+  @import '~@/assets/styles/theme'
+
+  .LayoutHeader
+    width: 100%
+    height: 98px
+    padding: 0 12px
+    border-bottom: 2px solid $color-primary
+    background-color: #FFFFFF
+
+    > .container
+      width: 100%
+      height: 100%
+      max-width: 1050px
+      display: flex
+      align-items: center
+      justify-content: flex-start
+      margin-left: auto
+      margin-right: auto
+
+      > .button
+        margin-left: auto
+
+      > .figure
+        height: 76px
+        margin-right: 12px
+</style>
+
