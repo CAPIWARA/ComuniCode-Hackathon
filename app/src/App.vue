fc8abb147b7dad23f4e5171117a3020f6de905db
Vitor Luiz Cavalcanti <vitorluizc@outlook.com>
2018-06-09 14:20:06 -0300

    Create Impact theme & ImpactButton

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,3 +1,17 @@
 <template>
   <router-view />
 </template>
+
+<style lang="stylus">
+  *
+    box-sizing: border-box
+    margin: 0
+    padding: 0
+
+  html
+    font-size: 62.5%
+
+  body
+    font-size: 1.8rem
+</style>
+
---
+++ app/src/components/Impact/ImpactButton.vue
@@ -0,0 +1,71 @@
+<template>
+  <component
+    v-bind="$attrs"
+    v-on="$listeners"
+    :class="classes"
+    :is="to ? 'router-link' : 'button'"
+    :to="to ? to : null"
+  >
+    <span class="text">
+      <slot />
+    </span>
+  </component>
+</template>
+
+<script>
+  export default {
+    props: {
+      to: {
+        type: [Object, String],
+        default: null
+      }
+    },
+    computed: {
+      classes () {
+        return [
+          'ImpactButton', {
+            '-is-link': this.to,
+            '-is-button': !this.to
+          }
+        ]
+      }
+    }
+  };
+</script>
+
+<style lang="stylus">
+  ImpactButton-color = #3A3A3A
+
+  .ImpactButton
+    display: flex
+    align-items: center
+    justify-content: center
+    height: 48px
+    padding-left: 12px
+    padding-right @padding-left
+    border: 2px solid ImpactButton-color
+    outline: none
+    cursor: pointer
+    transition: background-color .2s ease-in
+
+    > .text
+      text-align: center
+      text-transform: uppercase
+      letter-spacing: 1px
+      color: ImpactButton-color
+      transition: color .1s ease-in
+
+    &.-is-button
+      background-image: none
+      background-color: transparent
+
+    &.-is-link
+      text-decoration: none
+
+    &:hover
+      background-color: ImpactButton-color
+
+      > .text
+        color: #FFFFFF
+</style>
+
