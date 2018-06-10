66882d98ad5429110ad24e4f66a74713a53d7761
Marcos Vinicius Cardoso <ruizde169@gmail.com>
2018-06-10 09:53:43 -0300

    APRENDER A USAR A MERDA DO FRESH

--- server/users/token.go
+++ server/users/token.go
@@ -58,6 +58,8 @@ func Decode(tokenString string) (*jwtCustomClaims, error) {
 func (login *Login) Auth() (string, error) {
 	fmt.Printf("login: %v", login)
 	res, _ := FindByEmail(login.Email)
+	log.Printf("login password: %s", login.Password)
+	log.Printf("res password : %s", res.Password)
 	if res.Password == login.Password {
 		token, err := Encode(res.Id)
 		if err != nil {
